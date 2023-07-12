3055caf4e8280981f04a42a4e1d87c8dfb31d380
mateoU1297 <castro.mateo@correounivalle.edu.co>
2023-07-12 14:07:42 -0500

    se codifica la clave del token

--- src/main/java/com/springboot/app/msoauth/security/AuthorizationServerConfig.java
+++ src/main/java/com/springboot/app/msoauth/security/AuthorizationServerConfig.java
@@ -1,6 +1,7 @@
 package com.springboot.app.msoauth.security;
 
 import java.util.Arrays;
+import java.util.Base64;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -64,7 +65,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	@Bean
 	public JwtAccessTokenConverter accessTokenConverter() {
 		JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
-		tokenConverter.setSigningKey(env.getProperty("config.security.oauth.jwt.key"));
+		tokenConverter.setSigningKey(Base64.getEncoder().encodeToString(env.getProperty("config.security.oauth.jwt.key").getBytes()));
 		return tokenConverter;
 	}
 
