2c165b473550070bccc77904551619711b0ad556
mateoU1297 <castro.mateo@correounivalle.edu.co>
2023-07-12 02:28:27 -0500

    se agrega manejo de errores

--- src/main/java/com/springboot/app/msoauth/security/SpringSecurityConfig.java
+++ src/main/java/com/springboot/app/msoauth/security/SpringSecurityConfig.java
@@ -3,6 +3,7 @@ package com.springboot.app.msoauth.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationEventPublisher;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -15,6 +16,9 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private UserDetailsService usuarioService;
 
+	@Autowired
+	private AuthenticationEventPublisher eventPublisher;
+
 	@Bean
 	public BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
@@ -23,7 +27,8 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	@Autowired
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(this.usuarioService).passwordEncoder(passwordEncoder());
+		auth.userDetailsService(this.usuarioService).passwordEncoder(passwordEncoder()).and()
+				.authenticationEventPublisher(eventPublisher);
 	}
 
 	@Override
--- src/main/java/com/springboot/app/msoauth/services/UsuarioService.java
+++ src/main/java/com/springboot/app/msoauth/services/UsuarioService.java
@@ -17,6 +17,8 @@ import org.springframework.stereotype.Service;
 import com.springboot.app.msoauth.clients.UsuarioFeignClient;
 import com.springboot.app.msusuarioscommons.models.entity.Usuario;
 
+import feign.FeignException;
+
 @Service
 public class UsuarioService implements IUsuarioService, UserDetailsService {
 
@@ -28,22 +30,24 @@ public class UsuarioService implements IUsuarioService, UserDetailsService {
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
-		Usuario usuario = client.findByUsername(username);
+		try {
+			Usuario usuario = client.findByUsername(username);
 
-		if (usuario == null) {
-			log.error("Error en el login, no existe el usuario '" + username + "' en el sistema");
-			throw new UsernameNotFoundException(
-					"Error en el login, no existe el usuario '" + username + "' en el sistema");
+			List<GrantedAuthority> authorities = usuario.getRoles().stream()
+					.map(role -> new SimpleGrantedAuthority(role.getNombre()))
+					.peek(authority -> log.info("Role: " + authority.getAuthority())).collect(Collectors.toList());
+
+			log.info("Usuario autenticado: " + username);
+
+			return new User(usuario.getUsername(), usuario.getPassword(), usuario.getEnabled(), true, true, true,
+					authorities);
+
+		} catch (FeignException e) {
+			String error = "Error en el login, no existe el usuario '" + username + "' en el sistema";
+			log.error(error);
+
+			throw new UsernameNotFoundException(error);
 		}
-
-		List<GrantedAuthority> authorities = usuario.getRoles().stream()
-				.map(role -> new SimpleGrantedAuthority(role.getNombre()))
-				.peek(authority -> log.info("Role: " + authority.getAuthority())).collect(Collectors.toList());
-
-		log.info("Usario autenticado: " + username);
-
-		return new User(usuario.getUsername(), usuario.getPassword(), usuario.getEnabled(), true, true, true,
-				authorities);
 	}
 
 	@Override
